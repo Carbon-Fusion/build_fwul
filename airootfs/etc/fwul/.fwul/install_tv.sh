1ce4c798a6bfadbc1c52e420fb18fc3b7718296c
steadfasterX <steadfastX@boun.cr>
2017-01-16 17:33:36 +0100

    fix: remove warning when starting teamviewer

--- airootfs/etc/fwul/.fwul/install_tv.sh
+++ airootfs/etc/fwul/.fwul/install_tv.sh
@@ -3,4 +3,4 @@
 .fwul/install_package.sh yaourt teamviewer
 
 sudo systemctl start teamviewerd
-cp /usr/share/applications/com.teamviewer.TeamViewer.desktop ~/Desktop/
+cp /usr/share/applications/com.teamviewer.TeamViewer.desktop ~/Desktop/ && chmod +x ~/Desktop/com.teamviewer.TeamViewer.desktop
