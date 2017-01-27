0d6841cda3ef7d983235af1779134599e060cc0a
steadfasterX <steadfastX@boun.cr>
2017-01-27 12:46:17 +0100

    fix: error handling when installing fails
    now every step depends from the previous one which makes more sense

--- airootfs/etc/fwul/.fwul/install_tv.sh
+++ airootfs/etc/fwul/.fwul/install_tv.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-.fwul/install_package.sh yaourt teamviewer
-
-sudo systemctl start teamviewerd
-cp /usr/share/applications/com.teamviewer.TeamViewer.desktop ~/Desktop/ && chmod +x ~/Desktop/com.teamviewer.TeamViewer.desktop
+.fwul/install_package.sh yaourt teamviewer && sudo systemctl start teamviewerd && cp /usr/share/applications/com.teamviewer.TeamViewer.desktop ~/Desktop/ && chmod +x ~/Desktop/com.teamviewer.TeamViewer.desktop && rm ~/Desktop/install-TV.desktop
