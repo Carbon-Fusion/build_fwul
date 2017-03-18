82d9435de86744f3a64d1207c83c1662f183550f
steadfasterX <steadfastX@boun.cr>
2017-03-18 17:50:30 +0100

    new: make it fucking dynamic! oh i like it so much :)

--- airootfs/etc/fwul/.fwul/install_sonyflash.sh
+++ airootfs/etc/fwul/.fwul/install_sonyflash.sh
@@ -4,9 +4,13 @@ sudo pacman --noconfirm -Sy
 sudo yaourt --noconfirm -Sy
 pacman -Q xperia-flashtool
 if [ $? -ne 0 ];then
-    pacman -Q git-lfs || ~/.fwul/install_package.sh yaourt git-lfs
     [ -d /tmp/arch_xperia-flashtool ]&& rm -rf /tmp/arch_xperia-flashtool
-    cd /tmp && git clone https://github.com/steadfasterX/arch_xperia-flashtool.git && cd arch_xperia-flashtool && git lfs fetch
+    cd /tmp && git clone https://github.com/steadfasterX/arch_xperia-flashtool.git && cd arch_xperia-flashtool
+    
+    # this one is fun: we source the config from git and create the download URL + filename dynamically
+    # this way we do not need to build a new FWUL version when a new sony flashtool version will be released!
+    source PKGBUILD
+    wget "$downloadurl" -O flashtool-${pkgver}-linux.tar.7z
     wget http://www.flashtool.net/torrents/patches/linux/x10flasher.jar
     [ ! -f x10flasher.jar ]&& echo "ERROR downloading PATCH file" && exit 3
     pacman -Q libselinux || ~/.fwul/install_package.sh yaourt libselinux
