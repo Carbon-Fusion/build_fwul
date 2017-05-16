165efd4d6c6fc6d41dd0a465b116ea089690cba0
steadfasterX <steadfastX@boun.cr>
2017-05-16 16:34:03 +0200

    fix: fetching sony flashtool from AUR again
    as the source link in AUR is working again we will fetch
    and install with yaourt instead of the workaround used
    before.
    This fixes also an issue in i686 where sony flashtool
    could be installed but not started due to missing jre 32bit
    dependency (as removed in my workaround)
    
    Change-Id: Ia0778ddc4175711f11188d1ae0cfed0f361be4c5

--- airootfs/etc/fwul/.fwul/install_sonyflash.sh
+++ airootfs/etc/fwul/.fwul/install_sonyflash.sh
@@ -4,17 +4,18 @@ sudo pacman --noconfirm -Sy
 sudo yaourt --noconfirm -Sy
 pacman -Q xperia-flashtool
 if [ $? -ne 0 ];then
-    [ -d /tmp/arch_xperia-flashtool ]&& rm -rf /tmp/arch_xperia-flashtool
-    cd /tmp && git clone https://github.com/steadfasterX/arch_xperia-flashtool.git && cd arch_xperia-flashtool
+    #[ -d /tmp/arch_xperia-flashtool ]&& rm -rf /tmp/arch_xperia-flashtool
+    #cd /tmp && git clone https://github.com/steadfasterX/arch_xperia-flashtool.git && cd arch_xperia-flashtool
     
     # this one is fun: we source the config from git and create the download URL + filename dynamically
     # this way we do not need to build a new FWUL version when a new sony flashtool version will be released!
-    source PKGBUILD
-    wget "$downloadurl" -O flashtool-${pkgver}-linux.tar.7z
+    #source PKGBUILD
+    #wget "$downloadurl" -O flashtool-${pkgver}-linux.tar.7z
     wget http://www.flashtool.net/torrents/patches/linux/x10flasher.jar
     [ ! -f x10flasher.jar ]&& echo "ERROR downloading PATCH file" && exit 3
     pacman -Q libselinux || ~/.fwul/install_package.sh yaourt libselinux
-    makepkg -si
+    #makepkg -si
+    pacman -Q xperia-flashtool || ~/.fwul/install_package.sh yaourt xperia-flashtool
     sudo cp x10flasher.jar /usr/lib/xperia-flashtool/
 fi
 
