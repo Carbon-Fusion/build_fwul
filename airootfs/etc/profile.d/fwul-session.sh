deea9e817804ce8665f44b627d4e1e2d56fa40db
steadfasterX <steadfastX@boun.cr>
2017-09-08 14:38:49 +0200

    fixes #50
    detection is now for both the /tmp change and remount
    
    Change-Id: I211136610f569e409ae4c69663453da2078a0989

--- airootfs/etc/profile.d/fwul-session.sh
+++ airootfs/etc/profile.d/fwul-session.sh
@@ -28,22 +28,25 @@ sudo mv /tmp/locale.conf /etc/locale.conf
 ######
 # make more space available so we can install new software while running
 
+# detect persistent mode
+PERSMODE=0
+grep "cow_label=" /proc/cmdline >> /dev/null 2>&1
+[ $? -eq 0 ] && PERSMODE=1
+
 # get free mem info (we will not use % of free ram because it could make problems later)
 AVAILMEM=$(cat /proc/meminfo |grep MemFree| egrep -o '[0-9]*')
 # take 2/3 of the free ram and convert it to MB
 MEMMB=$((AVAILMEM*2/3/1024))
 
-# remount with the new size when the value is valid
-if [ "$MEMMB" -ge 700 ];then
-    sudo mount -o remount,size=${MEMMB}m /run/archiso/cowspace
+# remount (in forgetful mode only) with the new size when the value is valid
+if [ $PERSMODE -eq 0 ] && [ "$MEMMB" -ge 700 ];then
+  sudo mount -o remount,size=${MEMMB}m /run/archiso/cowspace
 fi
 
-# check if we run in persistent mode or not and unmount /tmp if RAM less than required min
-for opt in $(cat /proc/cmdline);do
-        if [ "${opt%=*}" == "cow_label" ] && [ "$MEMMB" -lt 1024 ];then
-            cp -a /tmp/* /var/tmp/
-            sudo umount -l /tmp
-            sudo mv /var/tmp/* /tmp/
-        fi
-done
+# when in persistent unmount /tmp if RAM less than required min
+if [ $PERSMODE -eq 1 ] && [ "$MEMMB" -lt 1024 ];then
+   cp -a /tmp/* /var/tmp/
+   sudo umount -l /tmp
+   sudo mv /var/tmp/* /tmp/
+fi
 
