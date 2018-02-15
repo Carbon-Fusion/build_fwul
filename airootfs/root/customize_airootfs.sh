7c6b4d1697dc971c8c2c66e3fc65b48f043eb1f6
steadfasterX <steadfastX@boun.cr>
2018-02-15 19:00:22 +0100

    buildnew: adding SALT version to output
    Change-Id: Ie3d04b9efee00e7b41206fbbe49e5bf563795d49
    buildfix: removed s-adb version string

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -742,9 +742,10 @@ expac -H M -s "%-30n %m" | sort -rhk 2 | head -n 40
 pacman --noconfirm -Rns expac
 
 # create a XDA copy template for the important FWUL package versions
-echo -ne '[*]Versions of the main FWUL components:\n[INDENT]\nKernel -> [B]'version:$(pacman -Qi linux-rt-manjaro |grep Version| cut -d ":" -f 2)'[/B]\nADB and fastboot: '
+echo -ne '[*]Versions of the main FWUL components:\n[INDENT]Kernel -> [B]'version:$(pacman -Qi linux-rt-manjaro |grep Version| cut -d ":" -f 2)'[/B]\nADB and fastboot: '
 pacman -Q android-tools | sed 's/ / -> [B]version: /g;s/$/[\/B]/g'
-echo -e 'simple-adb GUI -> [B]version: update4[/B]'
+echo -e 'simple-adb GUI -> [B]version: XXXXXXXXXXXXX[/B]'
+echo -e 'SALT -> [B]version: '$(egrep -o 'VERSION=.*' /home/$LOGINUSR/programs/SALT/salt.vars | cut -d '"' -f2) 
 CHLOG="heimdall-git xfwm4 lightdm xorg-server virtualbox-guest-utils firefox hexchat"
 for i in $CHLOG;do
         pacman -Q $i | sed 's/ / -> [B]version: /g;s/$/[\/B]/g'
