2c019b1a21c22c3f427eb40413e33efdd58403c9
steadfasterX <steadfastX@boun.cr>
2018-07-02 14:47:20 +0200

    issue #70: updated to 0.9.25.0
    added as workaround until fixed upstream
    
    Change-Id: I8ea5abf708617508b60f7a79038f9a367897e3b0

--- airootfs/etc/fwul/.fwul/install_sonyflash.sh
+++ airootfs/etc/fwul/.fwul/install_sonyflash.sh
@@ -4,7 +4,15 @@ sudo pacman --noconfirm -Sy
 sudo yaourt --noconfirm -Sy
 pacman -Q xperia-flashtool
 if [ $? -ne 0 ];then
-    sudo $HOME/.fwul/install_package.sh yaourt xperia-flashtool
+    #sudo $HOME/.fwul/install_package.sh yaourt xperia-flashtool
+
+    # upstream outdated! issue #70 workaround
+    cd /tmp/
+    wget http://leech.binbash.it:8008/misc/xperia-flashtool-interim.pkg.tar.xz -O xperia-flashtool-interim.pkg.tar.xz
+    pacman -Q libselinux || sudo $HOME/.fwul/install_package.sh yaourt libselinux
+    sudo pacman -S --noconfirm xperia-flashtool-interim.pkg.tar.xz 
+    rm xperia-flashtool-interim.pkg.tar.xz
+    # upstream outdated! issue #70 workaround
 fi
 
 pacman -Q libselinux && pacman -Q xperia-flashtool && cp ~/.fwul/sonyflash.desktop ~/Desktop/ && chmod +x ~/Desktop/sonyflash.desktop && rm ~/Desktop/install-sonyflash.desktop
