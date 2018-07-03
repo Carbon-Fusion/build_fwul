e1954a109975407527f31268b0308be71dc43a38
steadfasterX <steadfastX@boun.cr>
2018-07-03 12:28:55 +0200

    closes #74
    Change-Id: I21981d516743c40114ef3d6f3088046579a477b8

--- airootfs/etc/fwul/.fwul/install_tv.sh
+++ airootfs/etc/fwul/.fwul/install_tv.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+################################################################################################
+# simple wrapper to install latest compatible TV version
+################################################################################################
+source /etc/fwul-release
 
-sudo $HOME/.fwul/install_package.sh yaourt teamviewer && sudo systemctl start teamviewerd && cp /usr/share/applications/com.teamviewer.TeamViewer.desktop ~/Desktop/ && chmod +x ~/Desktop/com.teamviewer.TeamViewer.desktop && rm ~/Desktop/install-TV.desktop && sudo systemctl enable teamviewerd
+[ -d /tmp/tv ] && rm -rf /tmp/tv
+mkdir /tmp/tv && cd /tmp/tv \
+  && wget http://leech.binbash.it:8008/FWUL/.repo/tv_fwul-v${fwulversion}.tar -O tv_fwul-v${fwulversion}.tar \
+  && tar xf tv_fwul-v${fwulversion}.tar \
+  && sudo pacman -U --noconfirm *.pkg.tar.xz \
+  && sudo systemctl start teamviewerd \
+  && cp /usr/share/applications/com.teamviewer.TeamViewer.desktop ~/Desktop/ \
+  && chmod +x ~/Desktop/com.teamviewer.TeamViewer.desktop \
+  && rm ~/Desktop/install-TV.desktop \
+  && sudo systemctl enable teamviewerd
+
